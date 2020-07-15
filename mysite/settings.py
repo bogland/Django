9478c95210dccbc20f6b24390caca3cebcffd123
bogland <guruwang2@gmail.com>
2020-07-15 17:42:26 +0900

    댓글 구현 완료

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,6 +138,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 TAGGIT_CASE_INSENSITIVE = True
 TAGGIT_LIMIT = 50
 
-DISQUS_SHORTNAME = 'pydjango-web-programming'
-DISQUS_MY_DOMAIN = 'http://192.168.56.101:8000'
+DISQUS_SHORTNAME = 'hhhuh-com'
+DISQUS_MY_DOMAIN = 'http://127.0.0.1:8000'
 
